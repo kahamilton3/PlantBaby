78595df0a125e6ca93da80891c6106092d84dc08
Kirby <kafracas@gmail.com>
2021-01-06 16:28:22 -0600

    added plant type list and info

---
+++ PlantBaby/Controllers/PlantTypeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PlantBaby.Data;
+using PlantBaby.Models;
+
+namespace PlantBaby.Controllers
+{
+    public class PlantTypeController : Controller
+    {
+        private PlantDbContext context;
+
+        public PlantTypeController(PlantDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<PlantType> plantTypes = context.PlantTypes.ToList();
+            return View(plantTypes);
+        }
+
+        public IActionResult About(int id)
+        {
+            PlantType thePlantType = context.PlantTypes.Find(id);
+            return View(thePlantType);
+        }
+    }
+}
